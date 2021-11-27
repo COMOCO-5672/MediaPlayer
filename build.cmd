e712bb4064a22654e3c1ed679487d17de4ca02bc
COMOCO1 <775324623@qq.com>
2021-11-27 13:21:30 +0800

    修改build文件

--- build.cmd
+++ build.cmd
@@ -1,7 +1,10 @@
 @echo off
 title qmake and nmake build prompt
-set VCINSTALLDIR=D:\Program Files (x86)\Microsoft Visual Studio 14.0\VC
-set QTDIR=D:\Qt\Qt5.12.10\5.12.10\msvc2015_64
+set VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC
+set QTDIR=C:\Qt\Qt5.12.5\5.12.5\msvc2015_64
+
+rem set VCINSTALLDIR=D:\Program Files (x86)\Microsoft Visual Studio 14.0\VC
+rem set QTDIR=D:\Qt\Qt5.12.10\5.12.10\msvc2015_64
 set PATH=%VCINSTALLDIR%\bin;%QTDIR%\bin;%PATH%
 call "%VCINSTALLDIR%\vcvarsall.bat" amd64
 md bin
